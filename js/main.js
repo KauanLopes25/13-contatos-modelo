abb6652efcd77feefadcd0da0da20774b82fc5ef
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-11-17 08:53:06 -0300

    Adicionado função para adicionar novo contato pela pagina

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { lerContatos } from "./contatos.js"
+import { lerContatos, criarContato } from "./contatos.js"
 
 async function getUsersData() {
     const users = await lerContatos()
@@ -8,9 +8,9 @@ async function getUsersData() {
 }
 
 async function dataUserSeparation() {
-    const dataUserAll =  await getUsersData()
-    if(dataUserAll) {
-        for(let i = 0; i<= dataUserAll.length; i++){
+    const dataUserAll = await getUsersData()
+    if (dataUserAll) {
+        for (let i = 0; i <= dataUserAll.length; i++) {
             createUserCard(dataUserAll[i].foto, dataUserAll[i].nome, dataUserAll[i].celular)
         }
     } else {
@@ -19,7 +19,7 @@ async function dataUserSeparation() {
 }
 
 function createUserCard(dataImage, dataName, dataNumber) {
-    const cardContato = document.createElement('div') 
+    const cardContato = document.createElement('div')
     const image = document.createElement('img')
     const userName = document.createElement('h2')
     const number = document.createElement('p')
@@ -39,28 +39,44 @@ function createUserCard(dataImage, dataName, dataNumber) {
     console.log(getUsersData())
 }
 
-function accessToCreateNewUser(){
+function accessToCreateNewUser() {
     const main = document.querySelector(".card-show")
     main.className = "form-show"
 }
 
-function cancelCreateNewUser(){
+function cancelCreateNewUser() {
     const main = document.querySelector(".form-show")
     main.className = "card-show"
 }
 
 async function createNewUser() {
-    const name = document.querySelector("#nome")
-    const email = document.querySelector("#email")
-    const celular = document.querySelector('#celular')
-    const endereco = document.querySelector('#endereco')
-    const cidade = document.querySelector('#cidade')
+    const name = document.querySelector("#nome").value
+    const email = document.querySelector("#email").value
+    const phone = document.querySelector('#celular').value
+    const address = document.querySelector('#endereco').value
+    const city = document.querySelector('#cidade').value
+
+    const contact = {
+        "nome": name,
+        "celular": phone,
+        "foto": null,
+        "email": email,
+        "endereco": address,
+        "cidade": city
+    }
+
+    const createUser = await criarContato(contact)
+
+    const main = document.querySelector(".form-show")
+    main.className = "card-show"
 }
 
 const btnCreateNewUser = document.querySelector("#novo-contato")
+const btnSaveNewUser = document.querySelector("#salvar")
 const btnCancel = document.querySelector('#cancelar')
 
 btnCreateNewUser.addEventListener('click', accessToCreateNewUser)
+btnSaveNewUser.addEventListener('click', createNewUser)
 btnCancel.addEventListener('click', cancelCreateNewUser)
 
 dataUserSeparation()
